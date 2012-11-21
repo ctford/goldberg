22bd46eca7f86054f521799aca8c0214f51ca5d2
Chris Ford <christophertford@gmail.com>
2012-11-21 21:32:23 +0530

    Kill the dependency on seesaw because it errors.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "The Goldberg Variations in Overtone."
   :dependencies	[
     [org.clojure/clojure "1.4.0"]
-    [leipzig "0.1.0"]
-    [overtone "0.7.1"]
+    [leipzig "0.1.0" :exclusions [seesaw]]
+    [overtone "0.7.1" :exclusions [seesaw]]
   ]
 )
