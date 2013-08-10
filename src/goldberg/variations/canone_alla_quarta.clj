f45f6ac656d41349b8b43efc676ad2972f529a9e
Chris Ford <christophertford@gmail.com>
2013-08-10 13:48:49 +0300

    With the second part.

--- src/goldberg/variations/canone_alla_quarta.clj
+++ src/goldberg/variations/canone_alla_quarta.clj
@@ -69,7 +69,26 @@
          (then finale)
          (where :part (is :dux)))))
 
-(def melody2 [])
+(def melody2
+  (let [theme
+        (phrase (=> (repeats [[2 1/4] [1 1/2] [6 1/4] [1 5/4] [5 1/4] [1 1/2] [1 3/4] [3 1/4] [1 1/2] [1 1]]))
+                (=> (runs [[-3 -2 -6 -3] [-10 -9] [-11 -9 -10] [-8 -9 -8 -10 -9] [-4]])))
+        response
+        (phrase (repeats [[1 1/2] [12 1/4]])
+                (runs [[-9 -10 -9 -11 -2]]))
+        complicated 
+        (phrase (repeats [[1 7/2] [2 1/4] [3 1/2] [2 1/4] [1 2]])
+                (runs [[-2 -3 -2] [-2] [-2 -4 -3]]))
+        thence 
+        (phrase (repeats [[1 5/4] [11 1/4] [1 13/4]])
+                (runs [[-1 -3] [-1 -4] [-4 -8 -7]]))
+        blah 
+        (phrase (repeats [[11 1/4] [1 7/2] [3 1/2] [4 1/4] [4 1/2] [1 3/4] [1 1/4]])
+                (runs [[1 -1 0 -3 -2 -5 -4] [0 -3] [-5 -4] [-6 -4 -8]])) 
+        finale 
+        (phrase (repeats [[1 5/4] [11 1/4] [1 1/2] [1 3/4] [1 1/4] [1 1/2] [1 1]])
+                (runs [[-7] [-5 -12] [-10] [-7] [-5] [-3] [-7 -6] [-8 -7]]))] 
+    (->> theme (then response) (then complicated) (then thence) (then blah) (then finale))))
 
 (def bass1
   (let [crotchets-a
@@ -98,8 +117,24 @@
     (->> crotchets-a (then twiddle) (then crotchets-b) (then elaboration) (then busy) (then finale)
          (where :part (is :bass)))))
 
-(def bass2 [])
-
+(def bass2
+  (let [intro
+        (phrase (repeats [[3 1] [5 1/2] [2 1/4] [1 1/2] [1 1] [4 1/2] [1 7/2]])
+         (runs [[-10] [-10 -12 -11 -14 -11 -12 -11] [-9] [-13] [-11 -12]])) 
+        development 
+        (phrase (repeats [[5 1/2] [24 1/4]])
+         (runs [[-9 -3 -5 -4 -7 -6] [-8 -4 -6 -5] [-8] [-10] [-8] [-12] [-10 -12]]))
+        up-n-down 
+        (phrase (repeats [[8 1/4] [3 1/2] [1 3/4] [7 1/4] [1 1/2] [1 3/4] [7 1/4] [1 1/2] [1 3/4]])
+         (runs [[-9] [-11 -14] [-12] [-9 -10 -9 -11] [-4] [-9 -11 -10 -13 -12] [-5] [-10 -12 -11 -14 -13] [-6]]))
+        down 
+        (phrase (repeats [[27 1/4] [1 5/4] [3 1/4] [1 3/2]])
+         (runs [[-5 -7 -6 -9 -8 -11 -10 -13 -12 -15 -14] [-6 -8 -7 -10 -9 -11] [-9 -10]])) 
+        finale 
+        (phrase (repeats [[1 2] [3 1/2] [1 5/4] [3 1/4] [1 1]])
+         (runs [[-13] [-9] [-11 -10] [-14] [-12] [-10] [-8 -7]]))] 
+        (->> intro (then development) (then up-n-down) (then down) (then finale))
+    ))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Canone alla quarta - Johann Sebastian Bach   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
